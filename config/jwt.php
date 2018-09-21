91c5730f5f9038c8d2357a24e593e0f09c89cc26
juan vargas <juanvargasva.seo@gmail.com>
2018-09-21 10:16:54 -0500

    first commit

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'tlKxmlOawKXocr6dcsbBtlo0Od88TA9F'),
+    'secret' => env('JWT_SECRET', 'Wo9taqVIs8LxNwq6aGMHCzhQbtBJZdES'),
 
     /*
     |--------------------------------------------------------------------------
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +19,83 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
+
+
+
+Route::get('record/{id}',function($id){
+	$record = App\Record::findOrFail($id);
+	return json_encode(["record"=>$record,"message"=>"Record consultado con id = {$id}"]);
+});
+
+
+Route::post('rec',function(Request $request){
+
+		$rules =[
+	    'temp' => 'required|digits_between:1,5|numeric',
+	    'humidity' => 'required|digits_between:1,5|numeric',
+	    'co2' => 'required|digits_between:1,5|numeric',
+		];
+
+       $validator = \Validator::make($request->all(),$rules);
+
+		if ($validator->fails()) {
+		   return response()->json($validator->errors(), 422);
+		}
+
+	$record = new App\Record;
+	$record->temp = $request->input('temp');
+	$record->humidity = $request->input('humidity');
+	$record->co2 = $request->input('co2');
+	$record->save();
+	return json_encode(["record"=>$record,"message"=>"Registrado correctamente!"]);
+});
+
+
+
+
+Route::get('records',function(){
+	$records = App\Record::orderBy('id', 'ASC')->get();
+	return json_encode($records);
+});
+
+
+
+
+
+Route::put('record/{id}',function(Request $request,$id){
+
+	$rules =[
+	    'temp' => 'required|digits_between:1,5|numeric',
+	    'humidity' => 'required|digits_between:1,5|numeric',
+	    'co2' => 'required|digits_between:1,5|numeric',
+		];
+
+       $validator = \Validator::make($request->all(),$rules);
+
+		if ($validator->fails()) {
+		   return response()->json($validator->errors(), 422);
+		}
+
+
+	$record = App\Record::findOrFail($id);
+	$record->temp = $request->input('temp');
+	$record->humidity = $request->input('humidity');
+	$record->co2 = $request->input('co2');
+	$record->update();
+	return json_encode($record);
+});
+
+Route::delete('record/{id}',function($id){
+	$record = App\Record::findOrFail($id);
+	$record->delete();
+	return json_encode(['message'=>"Tu record fue eliminado!"]);
+});
+
+
+
 Route::middleware(['jwt.auth'])->group(function(){
 	Route::get('users','UserController@index');
+
 });
 
 
